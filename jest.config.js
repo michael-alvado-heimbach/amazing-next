7172763a3f2ee26d8ab6de1ee816984411a72697
Michael Alvado Heimbach <michael@xcidic.com>
2019-07-07 01:19:51 +0700

    added husky added jest for css modules

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 module.exports = {
-	testPathIgnorePatterns: ["<rootDir>/.next/","<rootDir>/out/","<rootDir>/pages", "<rootDir>/node_modules/"],
-	collectCoverage: true
+	testPathIgnorePatterns: [
+		"<rootDir>/.next/",
+		"<rootDir>/out/",
+		"<rootDir>/pages",
+		"<rootDir>/node_modules/"
+	],
+	collectCoverage: true,
+	moduleNameMapper: {
+		"\\.(css|less|scss|sss|styl)$": "<rootDir>/node_modules/jest-css-modules"
+	}
 };
