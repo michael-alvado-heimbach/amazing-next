9aede1ecb3e71f20e33e48071b3b589416116aba
Michael Alvado Heimbach <michael@xcidic.com>
2019-07-07 11:11:59 +0700

    added polyfills setup

--- next.config.js
+++ next.config.js
@@ -4,18 +4,35 @@ const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 module.exports = withCSS({
   cssModules: true,
   webpack: config => {
-    config.plugins.push(
-      new SWPrecacheWebpackPlugin({
-        verbose: true,
-        staticFileGlobsIgnorePatterns: [/\.next\//],
-        runtimeCaching: [
-          {
-            handler: 'networkFirst',
-            urlPattern: /^https?.*/,
-          },
-        ],
-      }),
-    );
+    config.plugins.push(serviceWorkerSetup());
+
+    const originalEntry = config.entry;
+    config.entry = async () => {
+      const entries = await originalEntry();
+      return polyfillSetup(entries);
+    };
+
     return config;
   },
 });
+
+function serviceWorkerSetup() {
+  const setup = new SWPrecacheWebpackPlugin({
+    verbose: true,
+    staticFileGlobsIgnorePatterns: [/\.next\//],
+    runtimeCaching: [
+      {
+        handler: 'networkFirst',
+        urlPattern: /^https?.*/,
+      },
+    ],
+  });
+  return setup;
+}
+
+async function polyfillSetup(entries) {
+  if (entries['main.js'] && !entries['main.js'].includes('./utils/polyfills.js')) {
+    entries['main.js'].unshift('./utils/polyfills.js');
+  }
+  return entries;
+}
---
+++ utils/polyfills.js
@@ -0,0 +1,10 @@
+/* eslint no-extend-native: 0 */
+import includes from 'core-js/library/fn/string/virtual/includes';
+import repeat from 'core-js/library/fn/string/virtual/repeat';
+import assign from 'core-js/library/fn/object/assign';
+
+console.log('Polyfills loaded');
+
+String.prototype.includes = includes;
+String.prototype.repeat = repeat;
+Object.assign = assign;
