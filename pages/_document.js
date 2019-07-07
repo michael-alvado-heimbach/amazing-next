2ad681bfae74db0ea90b290ce9767cd0739962cb
Michael Alvado Heimbach <michael@xcidic.com>
2019-07-07 17:59:49 +0700

    added load font roboto

--- pages/_document.js
+++ pages/_document.js
@@ -20,6 +20,7 @@ class MyDocument extends Document {
           <meta name='keywords' content='HTML,CSS,Javascript,Nextjs' />
           <meta name='theme-color' content={theme.palette.primary.main} />
           <link rel='manifest' href='/static/manifest.json' />
+          <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500&display=swap' />
         </Head>
         <body>
           <div id='app'>
